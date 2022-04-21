1abff19540c2ef3331e6e80052d3d558cb9ab0bb
prosper-20 <edwardprosper001@gmail.com>
2022-04-21 22:59:15 +0100

    updates

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('counter', views.counter, name='counter'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name="login")
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout")
 
 ]
