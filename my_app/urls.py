c07fc52ffc708d49334718b47a2a87c8bcaead12
prosper-20 <edwardprosper001@gmail.com>
2022-04-21 22:34:13 +0100

    updates

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,6 +4,7 @@ from .import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('counter', views.counter, name='counter'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name="login")
 
 ]
--- my_project/urls.py
+++ my_project/urls.py
@@ -20,4 +20,5 @@ from my_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('my_app.urls')),
+    
 ]
