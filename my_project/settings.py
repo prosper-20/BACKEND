07aef04428948f2145135f980e0f5429f9fcdd33
prosper-20 <edwardprosper001@gmail.com>
2022-04-22 10:56:45 +0100

    updates

--- my_project/settings.py
+++ my_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'initial',
+        'USER': 'postgres',
+        'PASSWORD': 'EddiePdgr8',
+        'HOST': 'localhost',
+        
     }
 }
 
